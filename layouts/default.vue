fd042d4b49e26bf1bb2a7ee9f599be85a0c96c65
Islom Murodov <brilliantpul@gmail.com>
2024-05-10 14:36:09 +0500

    Init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,18 @@
+<script setup lang="ts">
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
+
 <template>
-  <main>
-    <AppNavbar />
-    <div mx-auto max-w-7xl gap-5 lg:grid lg:grid-cols-5>
-      <div lg:col-span-4>
-        <slot />
+  <SpeedInsights>
+    <main>
+      <AppNavbar />
+      <div mx-auto max-w-7xl gap-5 lg:grid lg:grid-cols-5>
+        <div lg:col-span-4>
+          <slot />
+        </div>
+        <LazyRandomFilms lg:col-span-1 />
       </div>
-      <LazyRandomFilms lg:col-span-1 />
-    </div>
-    <AppFooter />
-  </main>
+      <AppFooter />
+    </main>
+  </SpeedInsights>
 </template>
