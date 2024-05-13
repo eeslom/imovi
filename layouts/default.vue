975dc7aa1c09fdd8761fe2f1d6ea338bab1f257b
Islom Murodov <brilliantpul@gmail.com>
2024-05-14 02:25:06 +0500

    Init

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,10 @@
 <template>
   <main>
+    <div mx-auto max-w-6xl px-2 py-1>
+      <marquee>
+        Sayt vaqtinchalik test rejimida ishlayabdi! Сайт работает во временном тестовом режиме!
+      </marquee>
+    </div>
     <AppNavbar />
     <div mx-auto max-w-7xl gap-5 px-3 py-4 xl:grid xl:grid-cols-5>
       <div xl:col-span-4>
