3ec5d9e3d9eeb98a55675bdebb82f50bb9b30a53
Islom Murodov <brilliantpul@gmail.com>
2024-05-19 23:39:09 +0500

    Init

--- pages/watch/[id].vue
+++ pages/watch/[id].vue
@@ -24,7 +24,7 @@ useSeoMeta({
     <TheDetails :item="item" />
     <HeadlessTabGroup as="div" mt-5>
       <HeadlessTabList>
-        <HeadlessTab v-slot="{ selected }" as="template">
+        <HeadlessTab v-if="item.video" v-slot="{ selected }" as="template">
           <button rounded-t px-4 py-2 text-sm font-semibold uppercase outline-none :class="[selected ? 'bg-light-gray' : 'bg-dark-gray']">
             onlayn ko'rish
           </button>
@@ -36,7 +36,7 @@ useSeoMeta({
         </HeadlessTab>
       </HeadlessTabList>
       <HeadlessTabPanels>
-        <HeadlessTabPanel>
+        <HeadlessTabPanel v-if="item.video">
           <iframe :src="item.video" frameborder="0" class="w-9/10" allowfullscreen aspect-ratio-video w-full />
         </HeadlessTabPanel>
         <HeadlessTabPanel v-if="item.trailer">
