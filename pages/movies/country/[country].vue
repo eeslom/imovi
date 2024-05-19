5354d5025d0ffc80c360285387f20f41cc496ae9
Islom Murodov <brilliantpul@gmail.com>
2024-05-19 17:23:27 +0500

    Init

--- pages/movies/country/[country].vue
+++ pages/movies/country/[country].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 const route = useRoute()
 
-const country = route.params.country
+const { country } = route.params as { country: string }
 
 const { data: items } = await useFetch(`/api/movies/country/${country}`)
 </script>
